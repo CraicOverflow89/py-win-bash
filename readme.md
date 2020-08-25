253dc225993b93447f4b4fd3daf93d1fde92de11
CraicOverflow89 <craic.overflow@hotmail.com>
2020-08-25 19:10:06 +0100

    Correct ordering for ls command.

--- readme.md
+++ readme.md
@@ -2,5 +2,3 @@ Windows Commands
 ================
 
 A few additional commands to use in [cmd](https://en.wikipedia.org/wiki/Cmd.exe) / [PowerShell](https://en.wikipedia.org/wiki/PowerShell), inspired by [Bash](https://www.gnu.org/software/bash/). Written in Python 3.
-
-![example](https://raw.githubusercontent.com/CraicOverflow89/win-commands/master/res/screenshots/001.PNG)
--- src/ls.py
+++ src/ls.py
@@ -1,3 +1,4 @@
+from math import ceil
 import os, shutil
 
 # Output Size
@@ -11,28 +12,29 @@ for file in file_list:
 		name_width = len(file)
 
 # Pad Strings
-file_list = map(lambda it: it.ljust(name_width), file_list)
+file_list = list(map(lambda it: it.ljust(name_width), file_list))
 
-# Create List
-output = []
-line = ""
+# Calculate Dimensions
+column_count = int(size_cols / (name_width + 2))
+row_count = int(ceil(len(file_list) / column_count))
+final_row_cols = column_count - ((column_count * row_count) - len(file_list))
 
-# Iterate Files
+# Create Columns
+column_list = []
+data = []
 for file in file_list:
+	data.append(file)
+	if len(data) == row_count or (len(data) == row_count - 1 and len(column_list) >= final_row_cols):
+		column_list.append(data)
+		data = []
 
-	# Length Exceeded
-	if len(line) + len(file) > size_cols:
-		output.append(line)
-		line = ""
-
-	# Update Line
-	if(len(line) > 0):
-		line += "  "
-	line += file
-
-# Final Line
-output.append(line)
-
-# Render Output
-for line in output:
+# Print Rows
+for row in range(row_count):
+	line = ""
+	for column in range(column_count):
+		if row == row_count - 1 and column == final_row_cols:
+			break
+		if(len(line) > 0):
+			line += "  "
+		line += column_list[column][row]
 	print(line)
