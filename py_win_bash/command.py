8d3f736827c53c1a6958d8ffffbd7acf6afd7cbf
CraicOverflow89 <craic.overflow@hotmail.com>
2020-10-03 17:16:25 +0100

    Finished initial unit tests for Command class.

--- py_win_bash/command.py
+++ py_win_bash/command.py
@@ -26,24 +26,34 @@ class Command:
         # Iterate Arguments
         arg_list = iter(input)
 
-        def _find_dict_value(dict: Dict, key: str, value: str) -> Optional[str]:
+        def _find_nested_value(list: List, key: str, value: str) -> Optional[str]:
             """
-            Finds a value in a dictionary at 
+            Finds a nested value in a list of dictionaries
+            :param list: the list to search
+            :param key: the key of the dictionary in each list element to check
+            :param value: the value to match to the key in the dictionary
+            :return: title of matched element or None
             """
-            for entry in dict:
-                if entry[key] is value:
+            for entry in list:
+                if entry[key] == value:
                     return entry["title"]
             return None
 
-        def _find_in_schema(key: str, value: str):
+        def _find_in_schema(key: str, value: str) -> None:
+            """
+            Finds a related flag or kwarg based on command input value
+            :param key: the key ("title" or "short") to check
+            :param value: the value to match against
+            :return: None
+            """
 
             # Flag Argument
-            find_flag = _find_dict_value(match["flags"], key, value)
+            find_flag = _find_nested_value(match["flags"], key, value)
             if find_flag:
                 result["flags"].append(find_flag)
 
             # Keyword Argument
-            find_kwarg = _find_dict_value(match["kwargs"], key, value)
+            find_kwarg = _find_nested_value(match["kwargs"], key, value)
             if find_kwarg:
                 result["kwargs"][find_kwarg] = next(arg_list, None)
                 # TODO: consider raising if next is None
