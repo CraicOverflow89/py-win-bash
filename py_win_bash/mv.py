69bd975b4c2f7ae18bdb147326d3a1baa172ed17
CraicOverflow89 <craic.overflow@hotmail.com>
2020-12-03 15:56:07 +0000

    First draft of the mv command.

--- py_win_bash/mv.py
+++ py_win_bash/mv.py
@@ -1,1 +1,21 @@
 # mv source target
+
+import os, shutil, sys
+
+# Source File
+if len(sys.argv) < 2:
+    print("Must supply source file!")
+    exit()
+file_source = os.getcwd() + "/" + sys.argv[1]
+if not os.path.exists(file_source):
+    print("Source file does not exist!")
+    exit()
+
+# Target File
+if len(sys.argv) < 3:
+    print("Must supply target file!")
+    exit()
+file_target = sys.argv[2]
+
+# Move File
+shutil.move(file_source, file_target)
